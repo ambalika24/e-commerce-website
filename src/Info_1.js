fe35331b3b672a45241ad8403700748d90e225fc
ambalika24 <ambalikachoubey888@gmail.com>
2021-06-24 19:28:07 +0530

    final changes

--- src/Info_1.js
+++ src/Info_1.js
@@ -58,7 +58,7 @@ import { db } from "./firebase";
         <div className="info">
             <Link to="/">
           <img className="login__logo" 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+          src=".\6.png" />
         </Link>
         <div className="info__container">
             <h1>Questionnaire</h1>
--- src/Info_2.js
+++ src/Info_2.js
@@ -58,7 +58,7 @@ import { db } from "./firebase";
         <div className="info">
             <Link to="/">
           <img className="login__logo" 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+          src=".\6.png" />
         </Link>
         <div className="info__container">
             <h1>Questionnaire</h1>
@@ -95,7 +95,7 @@ import { db } from "./firebase";
                 <center><button type="submit"
                 className="info__signInButton">Done</button></center>
             </form>
-            <p><input type="checkbox" required/> I agree to Amazon's conditions of use & sale. Please see our Privacy
+            <p><input type="checkbox" required/> I agree to StayWell's conditions of use & sale. Please see our Privacy
                 Notice, our Cookies Notice and our Interest-Based Ads Notice.
             </p>
             
